d36fc97735ff2fbc8917421016f64748a65a417c
htduong <21521988@gm.uit.edu.vn>
2023-11-01 21:29:34 +0700

    ab

--- app/src/main/java/com/doan/chatapp/SplashScreen.java
+++ app/src/main/java/com/doan/chatapp/SplashScreen.java
@@ -2,7 +2,9 @@ package com.doan.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,Login.class));
+                finish();
+            }
+        },2000);
     }
 }
