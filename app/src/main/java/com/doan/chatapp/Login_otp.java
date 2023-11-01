8dbbd51e1df12f65345b91631104cb9c00b2bb20
htduong <21521988@gm.uit.edu.vn>
2023-11-01 23:12:45 +0700

    ab

--- app/src/main/java/com/doan/chatapp/Login_otp.java
+++ app/src/main/java/com/doan/chatapp/Login_otp.java
@@ -3,12 +3,27 @@ package com.doan.chatapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Login_otp extends AppCompatActivity {
+
+    String phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_otp);
+
+        phoneNumber = getIntent().getExtras().getString("phone");
+        Toast.makeText(getApplicationContext(), phoneNumber, Toast.LENGTH_LONG).show();
+
+        Map<String, String> data = new HashMap<>();
+        FirebaseFirestore.getInstance().collection("test").add(data);
     }
 }
